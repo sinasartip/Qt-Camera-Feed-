48e2a107f9e0fbb81b82bfc8bce1b30f915d527d
Sina Sartip <ssartipz@ryerson.ca>
2020-07-31 02:36:26 -0400

    fixed multi threading

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import PySide2
 from PySide2 import QtWidgets
-from PySide2.QtCore import QThread, SIGNAL
+from PySide2.QtCore import QThread, SIGNAL, Signal, Slot, QObject
+
 import Ui_main
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
@@ -9,10 +10,15 @@ import numpy as np
 import queue
 from threading import Thread
 
+class signals(QObject):
+    image = Signal(object)
+
+
 class PlotThread(QThread):
-    def __init__(self,GUI,imgQueue):
+    def __init__(self,GUI):
         QThread.__init__(self)
         self.GUI = GUI
+        self.signal = signals()
 
         #setup of graphing widget
         self.graphWidget = pg.ImageView()
@@ -21,7 +27,7 @@ class PlotThread(QThread):
 
         #flags & queues
         self.stopped = False
-        self.imgQueue = imgQueue
+        
         
         
         
@@ -30,30 +36,27 @@ class PlotThread(QThread):
     def __del__(self):
         self.wait()
 
-    def run(self):
-        
+   
+    def run(self,image):
 
         #setup flag
         self.stopped = False
 
         #continoulsy grab images 
-        while not self.stopped:
-            #print("plotting")
-            self.graphWidget.setImage(self.imgQueue.get())
-            self.sleep(0.1)
-       
-
-        print("Exited thread graph!")
+        self.graphWidget.setImage(image)
 
 class camThread(QThread):
-    def __init__(self, imgQueue):
+    def __init__(self):
         QThread.__init__(self)
         self.stopped = False
-        self.imgQueue = imgQueue
+        self.signal = signals()
+        
+        
 
     def __del__(self):
         self.wait()
 
+    @Slot()
     def run(self):
         #setup flag
         self.stopped = False
@@ -62,46 +65,44 @@ class camThread(QThread):
         while(not self.stopped):
             # Capture frame-by-frame
             _, frame = cap.read()
-           
-            self.imgQueue.put(frame)
-            #print("capturing")
+        
+            self.signal.image.emit(frame)
+            
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 pass
 
         # When everything done, release the capture
         cap.release()
         print("Exited thread capture!")
+
+
         
-
-
 class GraphingApp(Ui_main.Ui_MainWindow, QtWidgets.QMainWindow):
     def __init__(self):
         super(GraphingApp,self).__init__()
         self.setupUi(self)
-
+        
+        
         #flags
         self.stopped = False
         
         ###page 1###
         
         #init graphing widget
-        self.imgQueue = queue.Queue(5)
+      
         
-        self.plot_thread = PlotThread(self.mainPageGrid,self.imgQueue)
+        self.plot_thread = PlotThread(self.mainPageGrid)
 
         #setup signals and slotes 
-        self.cam_thread = camThread(self.imgQueue) #camera feed thread
-        self.connect(self.cam_thread,SIGNAL("started()"), self.plot_thread.start()) #camera feed signals plotting thread
-        self.StartPB.clicked.connect(self.cam_thread.start) #start push button sends camera feed signal
+        self.cam_thread = camThread() #camera feed thread
+        self.StartPB.clicked.connect(self.cam_thread.start)#start push button sends camera feed signal
+        self.cam_thread.signal.image.connect(self.plot_thread.run)
         self.StopPB.clicked.connect(self.stop) 
     
     def stop(self): 
         self.stopped = True
         self.cam_thread.stopped = True
         self.plot_thread.stopped = True
-
-    
-                
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication()
